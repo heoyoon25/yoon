a03da140d1dee8cb5386838386947a6d27c89d1b
heoyoon25 <heoyoon25@gmail.com>
2025-09-17 16:59:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import streamlit as st
 st.title('연산')
-
 st.header('두 숫자 더하기')
-col1, col2 = st.columns(2)
 
-with col1:
-    A = st.text_input('첫 번째 숫자 (A)를 입력하세요:', key='A_input')
-with col2:
-    B = st.text_input('두 번째 숫자 (B)를 입력하세요:', key='B_input')
-try:
-    num_A = int(A)
-    num_B = int(B)
-    sum_result = num_A + num_B
-    st.success(f'두 수의 합은?{sum_result}')
+A = input("숫자 A를 입력하세요: ")
 
-except ValueError:
-    st.error('유효한 숫자를 입력해 주세요')
+B = input("숫자 B를 입력하세요: ")
 
+sum_result = int(A) + int(B)
+
+print(f"두 수의 합은: {sum_result}")
 
 st.markdown('---')
 st.header('선택된 숫자까지의 합계 계산')
