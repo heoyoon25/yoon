c981cce846552cb5ed7c76ab545e16a58369ac36
heoyoon25 <heoyoon25@gmail.com>
2025-09-17 17:02:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import streamlit as st
 st.title('연산')
 st.header('두 숫자 더하기')
 
-A = input("숫자 A를 입력하세요: ")
+num_a = st.number_input("숫자 A를 입력하세요", value=0, step=1)
 
-B = input("숫자 B를 입력하세요: ")
+num_b = st.number_input("숫자 B를 입력하세요", value=0, step=1)
 
-sum_result = int(A) + int(B)
+sum_result = num_a + num_b
 
-print(f"두 수의 합은: {sum_result}")
+st.write(f"두 수의 합은: {sum_result}")
 
 st.markdown('---')
 
